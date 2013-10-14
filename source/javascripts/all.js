9a9c3d7529af582a12effefe455008fccf735502
Shawn Leslie <shawntleslie@gmail.com>
2013-10-14 11:48:45 -0700

    Finalized RWD Styling for home template and JS help from John

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,26 @@
 //= require_tree .
+
+$(function() {
+
+  var showNotes = $('#show-notes-container');
+
+  showNotes.data('default-height', showNotes.height()).height(0);
+
+  $('#notes').on('click', function(event) {
+    event.preventDefault();
+    if( $(this).hasClass('show') ){
+      // Remove Class Show
+      showNotes.height(0);
+      $('div.show-notes').removeClass('show');
+      $(this).removeClass('show');
+      $(this).find('span').html($(this).data('default-text'));
+    }else{
+      // Add Class Show
+      showNotes.height(showNotes.data('default-height'));
+      $('div.show-notes').addClass('show');
+      $(this).addClass('show');
+      $(this).find('span').html($(this).data('hidden-text'));
+    }
+  });
+
+});
